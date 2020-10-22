7e1895939af62adcc30ad750eef537fba33f3593
Damien <damien.voelker@googlemail.com>
2020-10-22 09:26:09 +0200

    first steps with firebase login

--- app/src/main/java/de/codingkeks/shoppinglist/MainActivity.kt
+++ app/src/main/java/de/codingkeks/shoppinglist/MainActivity.kt
@@ -1,6 +1,9 @@
 package de.codingkeks.shoppinglist
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -14,10 +17,14 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val RC_AUTH = 20;
+    private val TAG = "shoppinglist_1234abcd"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +60,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
+        Log.d(TAG, "onStart_Start")
         super.onStart()
 
+        //authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        Log.d(TAG, "startActivityForResult_Pre")
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_AUTH
+        )
+        Log.d(TAG, "startActivityForResult_Post")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "onActivityResult_Start")
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_AUTH) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                println(user?.email);
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+        Log.d(TAG, "onActivityResult_End")
     }
 }
