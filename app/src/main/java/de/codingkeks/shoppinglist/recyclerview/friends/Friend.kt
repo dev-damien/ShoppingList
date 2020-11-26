77c4d279cb4730ae32f145ffb27b3c0354717308
Severin <severin.hanke@th-bingen.de>
2020-11-26 17:04:53 +0100

    - Testing RecyclerView (DO NOT USE ITS RIPPED)

--- app/src/main/java/de/codingkeks/shoppinglist/recyclerview/friends/Friend.kt
+++ app/src/main/java/de/codingkeks/shoppinglist/recyclerview/friends/Friend.kt
@@ -2,6 +2,6 @@ package de.codingkeks.shoppinglist.recyclerview.friends
 
 data class Friend(
     var name: String,
-    var imageView: Int,
-    var favorite:Boolean
+    var profilePicture: Int,
+    var isFavorite:Boolean
 )
--- app/src/main/java/de/codingkeks/shoppinglist/recyclerview/friends/FriendAdapter.kt
+++ app/src/main/java/de/codingkeks/shoppinglist/recyclerview/friends/FriendAdapter.kt
@@ -1,10 +1,13 @@
 package de.codingkeks.shoppinglist.recyclerview.friends
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import de.codingkeks.shoppinglist.R
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import kotlinx.android.synthetic.main.rv_friend.view.*
 
 class FriendAdapter(var friends: List<Friend>): RecyclerView.Adapter<FriendAdapter.FriendViewHolder>()  {
     inner class FriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
@@ -16,7 +19,14 @@ class FriendAdapter(var friends: List<Friend>): RecyclerView.Adapter<FriendAdapt
 
     override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
         holder.itemView.apply {
-            //TODO Variablen binden
+            tvFriendName.text = friends[position].name
+            ivFriendImage.setImageResource(friends[position].profilePicture)
+            if (friends[position].isFavorite) ivFriendFav.setImageResource(R.drawable.ic_friends_star)
+            else ivFriendFav.setImageResource(R.drawable.ic_friends_star_border)
+
+            ivFriendFav.setOnClickListener() {
+                friends[position].isFavorite = !friends[position].isFavorite
+            }
         }
     }
 
