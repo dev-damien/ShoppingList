3a9076e77fbecd91f5ebdeea3879a1d1bf64d9eb
HankeKun <severin.hanke@outlook.de>
2021-03-05 15:02:35 +0100

    created ItemsActivity.kt and the RecyclerView for the items page

---
+++ app/src/main/java/de/codingkeks/shoppinglist/ItemsActivity.kt
@@ -0,0 +1,13 @@
+package de.codingkeks.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_items)
+
+
+    }
+}
