db5cd8b6e7b6aeab00d19364c6f60c29985f9f45
Severin <severin.hanke@th-bingen.de>
2021-03-10 13:51:31 +0100

    Some minor changes

--- app/src/main/java/de/codingkeks/shoppinglist/ui/friends/FriendsRequestsFragment.kt
+++ app/src/main/java/de/codingkeks/shoppinglist/ui/friends/FriendsRequestsFragment.kt
@@ -2,10 +2,10 @@ package de.codingkeks.shoppinglist.ui.friends
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
@@ -74,7 +74,6 @@ class FriendsRequestsFragment : Fragment() {
             tvFriendRequestsNoFriends.text = ""
 
             //get requested user data and add to the list
-            //friendRequestsList.clear()
             db.collection("users")
                 .whereIn(FieldPath.documentId(), friendRequestIds).get()
                 .addOnSuccessListener { userDocs  ->
