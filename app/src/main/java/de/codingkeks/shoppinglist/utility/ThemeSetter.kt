9f7bb36602e9603f3e55232acea0ab4c20e86170
Damien <damien.voelker@googlemail.com>
2021-03-18 11:20:27 +0100

    fixed ThemeSetter.kt

--- app/src/main/java/de/codingkeks/shoppinglist/utility/ThemeSetter.kt
+++ app/src/main/java/de/codingkeks/shoppinglist/utility/ThemeSetter.kt
@@ -8,16 +8,16 @@ import kotlinx.coroutines.launch
 
 open class ThemeSetter: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         lifecycleScope.launch {
             when (DataStoreUtility.readInt("theme", this@ThemeSetter)) { //0: Light, 1: Dark
                 0 -> {
                     setTheme(R.style.AppTheme)
                 }
                 1 -> {
-                    setTheme(R.style.AppTheme)
+                    setTheme(R.style.AppThemeDark)
                 }
             }
         }
+        super.onCreate(savedInstanceState)
     }
 }
